cf43bf86264b2dadf5d67981f37900f7fa00069c
Zach Schneider <me@zachschneider.com>
2021-10-25 11:21:19 -0500

    https api

--- src/api/openWeatherAPI.js
+++ src/api/openWeatherAPI.js
@@ -4,7 +4,7 @@ export const FETCH_WEATHER = "FETCH_WEATHER";
 const units = "imperial";
 
 export function fetchWeather(city) {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${process
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${process
     .env.REACT_APP_OPEN_WEATHER_API_KEY}`;
   const request = axios.get(url);
 
