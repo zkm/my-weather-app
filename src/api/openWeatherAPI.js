b9ef376f09e9aa3da73af9481117dbd1ee4b42e0
Zach Schneider <me@zachschneider.com>
2021-10-29 12:01:06 -0500

    adding try/catch on api call

--- src/api/openWeatherAPI.js
+++ src/api/openWeatherAPI.js
@@ -4,12 +4,25 @@ export const FETCH_WEATHER = "FETCH_WEATHER";
 const units = "imperial";
 
 export function fetchWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${process
-    .env.REACT_APP_OPEN_WEATHER_API_KEY}`;
-  const request = axios.get(url);
+  const weatherAPI = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${weatherAPI}`;
+  // const request = axios.get(url)
+  return axios.get(url)
+  .then(({data}) => {
+    // handle success
+    console.log(data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
-  return {
-    type: "FETCH_WEATHER",
-    payload: request
-  };
+  // return {
+  //   type: "FETCH_WEATHER",
+  //   payload: request
+  // };
 }
+
